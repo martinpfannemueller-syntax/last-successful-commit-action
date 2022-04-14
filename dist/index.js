571e660f12e1fe42393066dc5eb24d0974a539a3
Martin Pfannemüller <martin.pfannemueller@syntax.com>
2022-04-14 15:34:55 +0200

    Sort workflow_runs by update_at property

--- dist/index.js
+++ dist/index.js
@@ -8484,9 +8484,13 @@ try {
       event: "push",
     })
     .then((res) => {
+      let workflow_runs = res.data.workflow_runs;
+      workflow_runs.sort(function (a, b) {
+        return new Date(b.updated_at) - new Date(a.updated_at);
+      });
       const lastSuccessCommitHash =
-        res.data.workflow_runs.length > 0
-          ? res.data.workflow_runs[0].head_commit.id
+        workflow_runs.length > 0
+          ? workflow_runs[0].head_commit.id
           : "";
       core.setOutput("commit_hash", lastSuccessCommitHash);
     })
